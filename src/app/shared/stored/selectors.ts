471505c5db3eea1ac1f93e006e464826ab67bd0a
adrian.comandi <adrian.garcia@visma.com>
2021-10-27 11:24:49 +0200

    Quick fixes

--- src/app/shared/stored/selectors.ts
+++ src/app/shared/stored/selectors.ts
@@ -4,6 +4,5 @@ import { PostStateInterface } from "./states";
 const getPostState = createFeatureSelector<PostStateInterface>('post');
 
 export const getPosts = createSelector(getPostState, (state) => {
-    console.log("GET POST", state, state.post)
     return state.post;
 });
